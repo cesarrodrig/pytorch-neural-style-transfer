41e9ec57b419377229af55165ef54dc8d3519371
Cesar Rodriguez <cesar@dnlab.de>
2022-02-03 19:46:20 +0100

    Run formatter

--- models/definitions/vgg_nets.py
+++ models/definitions/vgg_nets.py
@@ -4,19 +4,24 @@ from torchvision import models
 
 """
     More detail about the VGG architecture (if you want to understand magic/hardcoded numbers) can be found here:
-    
+
     https://github.com/pytorch/vision/blob/3c254fb7af5f8af252c24e89949c54a3461ff0be/torchvision/models/vgg.py
 """
 
 
 class Vgg16(torch.nn.Module):
     """Only those layers are exposed which have already proven to work nicely."""
+
     def __init__(self, requires_grad=False, show_progress=False):
         super().__init__()
-        vgg_pretrained_features = models.vgg16(pretrained=True, progress=show_progress).features
-        self.layer_names = ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3']
+        vgg_pretrained_features = models.vgg16(
+            pretrained=True, progress=show_progress
+        ).features
+        self.layer_names = ["relu1_2", "relu2_2", "relu3_3", "relu4_3"]
         self.content_feature_maps_index = 1  # relu2_2
-        self.style_feature_maps_indices = list(range(len(self.layer_names)))  # all layers used for style representation
+        self.style_feature_maps_indices = list(
+            range(len(self.layer_names))
+        )  # all layers used for style representation
 
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
@@ -50,12 +55,26 @@ class Vgg16(torch.nn.Module):
 
 class Vgg16Experimental(torch.nn.Module):
     """Everything exposed so you can play with different combinations for style and content representation"""
+
     def __init__(self, requires_grad=False, show_progress=False):
         super().__init__()
-        vgg_pretrained_features = models.vgg16(pretrained=True, progress=show_progress).features
-        self.layer_names = ['relu1_1', 'relu2_1', 'relu2_2', 'relu3_1', 'relu3_2', 'relu4_1', 'relu4_3', 'relu5_1']
+        vgg_pretrained_features = models.vgg16(
+            pretrained=True, progress=show_progress
+        ).features
+        self.layer_names = [
+            "relu1_1",
+            "relu2_1",
+            "relu2_2",
+            "relu3_1",
+            "relu3_2",
+            "relu4_1",
+            "relu4_3",
+            "relu5_1",
+        ]
         self.content_feature_maps_index = 4
-        self.style_feature_maps_indices = list(range(len(self.layer_names)))  # all layers used for style representation
+        self.style_feature_maps_indices = list(
+            range(len(self.layer_names))
+        )  # all layers used for style representation
 
         self.conv1_1 = vgg_pretrained_features[0]
         self.relu1_1 = vgg_pretrained_features[1]
@@ -152,7 +171,9 @@ class Vgg16Experimental(torch.nn.Module):
         x = self.max_pooling5(x)
         # expose only the layers that you want to experiment with here
         vgg_outputs = namedtuple("VggOutputs", self.layer_names)
-        out = vgg_outputs(relu1_1, relu2_1, relu2_2, relu3_1, relu3_2, relu4_1, relu4_3, relu5_1)
+        out = vgg_outputs(
+            relu1_1, relu2_1, relu2_2, relu3_1, relu3_2, relu4_1, relu4_3, relu5_1
+        )
 
         return out
 
@@ -164,14 +185,31 @@ class Vgg19(torch.nn.Module):
     'conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1' were used for style representation
     'conv4_2' was used for content representation (although they did some experiments with conv2_2 and conv5_2)
     """
+
     def __init__(self, requires_grad=False, show_progress=False, use_relu=True):
         super().__init__()
-        vgg_pretrained_features = models.vgg19(pretrained=True, progress=show_progress).features
+        vgg_pretrained_features = models.vgg19(
+            pretrained=True, progress=show_progress
+        ).features
         if use_relu:  # use relu or as in original paper conv layers
-            self.layer_names = ['relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'conv4_2', 'relu5_1']
+            self.layer_names = [
+                "relu1_1",
+                "relu2_1",
+                "relu3_1",
+                "relu4_1",
+                "conv4_2",
+                "relu5_1",
+            ]
             self.offset = 1
         else:
-            self.layer_names = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv4_2', 'conv5_1']
+            self.layer_names = [
+                "conv1_1",
+                "conv2_1",
+                "conv3_1",
+                "conv4_1",
+                "conv4_2",
+                "conv5_1",
+            ]
             self.offset = 0
         self.content_feature_maps_index = 4  # conv4_2
         # all layers used for style representation except conv4_2
@@ -184,17 +222,17 @@ class Vgg19(torch.nn.Module):
         self.slice4 = torch.nn.Sequential()
         self.slice5 = torch.nn.Sequential()
         self.slice6 = torch.nn.Sequential()
-        for x in range(1+self.offset):
+        for x in range(1 + self.offset):
             self.slice1.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(1+self.offset, 6+self.offset):
+        for x in range(1 + self.offset, 6 + self.offset):
             self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(6+self.offset, 11+self.offset):
+        for x in range(6 + self.offset, 11 + self.offset):
             self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(11+self.offset, 20+self.offset):
+        for x in range(11 + self.offset, 20 + self.offset):
             self.slice4.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(20+self.offset, 22):
+        for x in range(20 + self.offset, 22):
             self.slice5.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(22, 29++self.offset):
+        for x in range(22, 29 + +self.offset):
             self.slice6.add_module(str(x), vgg_pretrained_features[x])
         if not requires_grad:
             for param in self.parameters():
--- utils/utils.py
+++ utils/utils.py
@@ -17,19 +17,26 @@ IMAGENET_STD_NEUTRAL = [1, 1, 1]
 # Image manipulation util functions
 #
 
+
 def load_image(img_path, target_shape=None):
     if not os.path.exists(img_path):
-        raise Exception(f'Path does not exist: {img_path}')
-    img = cv.imread(img_path)[:, :, ::-1]  # [:, :, ::-1] converts BGR (opencv format...) into RGB
+        raise Exception(f"Path does not exist: {img_path}")
+    img = cv.imread(img_path)[
+        :, :, ::-1
+    ]  # [:, :, ::-1] converts BGR (opencv format...) into RGB
 
     if target_shape is not None:  # resize section
-        if isinstance(target_shape, int) and target_shape != -1:  # scalar -> implicitly setting the height
+        if (
+            isinstance(target_shape, int) and target_shape != -1
+        ):  # scalar -> implicitly setting the height
             current_height, current_width = img.shape[:2]
             new_height = target_shape
             new_width = int(current_width * (new_height / current_height))
             img = cv.resize(img, (new_width, new_height), interpolation=cv.INTER_CUBIC)
         else:  # set both dimensions to target shape
-            img = cv.resize(img, (target_shape[1], target_shape[0]), interpolation=cv.INTER_CUBIC)
+            img = cv.resize(
+                img, (target_shape[1], target_shape[0]), interpolation=cv.INTER_CUBIC
+            )
 
     # this need to go after resizing - otherwise cv.resize will push values outside of [0,1] range
     img = img.astype(np.float32)  # convert from uint8 to float32
@@ -42,11 +49,13 @@ def prepare_img(img_path, target_shape, device):
 
     # normalize using ImageNet's mean
     # [0, 255] range worked much better for me than [0, 1] range (even though PyTorch models were trained on latter)
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255)),
-        transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255)),
+            transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL),
+        ]
+    )
 
     img = transform(img).to(device).unsqueeze(0)
 
@@ -56,31 +65,47 @@ def prepare_img(img_path, target_shape, device):
 def save_image(img, img_path):
     if len(img.shape) == 2:
         img = np.stack((img,) * 3, axis=-1)
-    cv.imwrite(img_path, img[:, :, ::-1])  # [:, :, ::-1] converts rgb into bgr (opencv contraint...)
+    cv.imwrite(
+        img_path, img[:, :, ::-1]
+    )  # [:, :, ::-1] converts rgb into bgr (opencv contraint...)
 
 
 def generate_out_img_name(config):
-    prefix = os.path.basename(config['content_img_name']).split('.')[0] + '_' + os.path.basename(config['style_img_name']).split('.')[0]
+    prefix = (
+        os.path.basename(config["content_img_name"]).split(".")[0]
+        + "_"
+        + os.path.basename(config["style_img_name"]).split(".")[0]
+    )
     # called from the reconstruction script
-    if 'reconstruct_script' in config:
+    if "reconstruct_script" in config:
         suffix = f'_o_{config["optimizer"]}_h_{str(config["height"])}_m_{config["model"]}{config["img_format"][1]}'
     else:
         suffix = f'_o_{config["optimizer"]}_i_{config["init_method"]}_h_{str(config["height"])}_m_{config["model"]}_cw_{config["content_weight"]}_sw_{config["style_weight"]}_tv_{config["tv_weight"]}{config["img_format"][1]}'
     return prefix + suffix
 
 
-def save_and_maybe_display(optimizing_img, dump_path, config, img_id, num_of_iterations, should_display=False):
-    saving_freq = config['saving_freq']
-    out_img = optimizing_img.squeeze(axis=0).to('cpu').detach().numpy()
-    out_img = np.moveaxis(out_img, 0, 2)  # swap channel from 1st to 3rd position: ch, _, _ -> _, _, chr
+def save_and_maybe_display(
+    optimizing_img, dump_path, config, img_id, num_of_iterations, should_display=False
+):
+    saving_freq = config["saving_freq"]
+    out_img = optimizing_img.squeeze(axis=0).to("cpu").detach().numpy()
+    out_img = np.moveaxis(
+        out_img, 0, 2
+    )  # swap channel from 1st to 3rd position: ch, _, _ -> _, _, chr
 
     # for saving_freq == -1 save only the final result (otherwise save with frequency saving_freq and save the last pic)
-    if img_id == num_of_iterations-1 or (saving_freq > 0 and img_id % saving_freq == 0):
-        img_format = config['img_format']
-        out_img_name = str(img_id).zfill(img_format[0]) + img_format[1] if saving_freq != -1 else generate_out_img_name(config)
+    if img_id == num_of_iterations - 1 or (
+        saving_freq > 0 and img_id % saving_freq == 0
+    ):
+        img_format = config["img_format"]
+        out_img_name = (
+            str(img_id).zfill(img_format[0]) + img_format[1]
+            if saving_freq != -1
+            else generate_out_img_name(config)
+        )
         dump_img = np.copy(out_img)
         dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
-        dump_img = np.clip(dump_img, 0, 255).astype('uint8')
+        dump_img = np.clip(dump_img, 0, 255).astype("uint8")
         cv.imwrite(os.path.join(dump_path, out_img_name), dump_img[:, :, ::-1])
 
     if should_display:
@@ -95,7 +120,7 @@ def get_uint8_range(x):
         x *= 255
         return x
     else:
-        raise ValueError(f'Expected numpy array got {type(x)}')
+        raise ValueError(f"Expected numpy array got {type(x)}")
 
 
 #
@@ -107,22 +132,25 @@ def get_uint8_range(x):
 def prepare_model(model, device):
     # we are not tuning model weights -> we are only tuning optimizing_img's pixels! (that's why requires_grad=False)
     experimental = False
-    if model == 'vgg16':
+    if model == "vgg16":
         if experimental:
             # much more flexible for experimenting with different style representations
             model = Vgg16Experimental(requires_grad=False, show_progress=True)
         else:
             model = Vgg16(requires_grad=False, show_progress=True)
-    elif model == 'vgg19':
+    elif model == "vgg19":
         model = Vgg19(requires_grad=False, show_progress=True)
     else:
-        raise ValueError(f'{model} not supported.')
+        raise ValueError(f"{model} not supported.")
 
     content_feature_maps_index = model.content_feature_maps_index
     style_feature_maps_indices = model.style_feature_maps_indices
     layer_names = model.layer_names
 
-    content_fms_index_name = (content_feature_maps_index, layer_names[content_feature_maps_index])
+    content_fms_index_name = (
+        content_feature_maps_index,
+        layer_names[content_feature_maps_index],
+    )
     style_fms_indices_names = (style_feature_maps_indices, layer_names)
     return model.to(device).eval(), content_fms_index_name, style_fms_indices_names
 
@@ -138,5 +166,6 @@ def gram_matrix(x, should_normalize=True):
 
 
 def total_variation(y):
-    return torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:])) + \
-           torch.sum(torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :]))
+    return torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:])) + torch.sum(
+        torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :])
+    )
